c53113823d4ebfc578eff316562ee2f7bc4a756c
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-18 13:44:42 +1000

    flask running okay

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+
+model = None
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=os.getenv('FLASK_PORT', 8000), host='0.0.0.0')
