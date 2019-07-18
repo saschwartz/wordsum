bbcf89ea30a7b21600f8a679e7d49441985cb0ae
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-18 15:30:10 +1000

    touchups

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,107 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang='pug'>
+  div#app
+    div#main-content-container
+      div#main-content-inner
+        div#page-content
+          h1 Wordsum
+          p#tagline
+            | Exploring the additive properties of 
+            a(href='https://en.wikipedia.org/wiki/Word2vec', target='_blank') word2vec
+          div#nav-container
+            hr
+            div#nav-links
+              div.nav-link
+                router-link(to='/wordsum') Wordsum
+              div.nav-link
+                router-link(to='/about') About
+            hr
+          transition(name="fade" mode="out-in")
+            router-view
+        Footer
 </template>
 
-<style>
+<script>
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: { Footer }
+}
+</script>
+
+<style lang='scss'>
+@import '~@/styles/fonts';
+@import '~@/styles/layout';
+@import '~@/styles/variables';
+
+/* transition styling */
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.2s ease;
+} 
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+  opacity: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  background: rgba($theme-secondary-color, 0.8);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#nav-container {
+  margin-bottom: 30px;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  #nav-links {
+    display: flex;
+    flex-wrap: wrap;
+    .nav-link {
+      margin-right: 25px;
+    }
+    .router-link-active {
+      font-weight: bold;
+      color: $theme-secondary-color;
+    }
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+// separate out page content
+#main-content-container {
+  margin: auto;
+  max-width: 1100px;
+  height: 100vh;
+
+  #main-content-inner {
+    overflow:auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    position: relative;
+    background: rgba(255, 255, 255, 0.92);
+    z-index: 10;
+    height: 100%;
+
+    #page-content {
+      padding-left: 3rem;
+      padding-right: 3rem;
+      margin-top: 30px;
+
+      h1 {
+        margin-bottom: 0px;
+      }
+
+      #tagline {
+        font-size: 0.9rem;
+      }
+    }
+  }
 }
+
 </style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template lang='pug'>
+  div#footer
+    div#inner-footer
+      div#copyright © {{ new Date().getFullYear() }} Sebastian Schwartz
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang='scss'>
+@import '~@/styles/fonts';
+@import '~@/styles/layout';
+@import '~@/styles/variables';
+
+#footer {
+  width: 100%;
+  margin-top: 2rem;
+  background-color: $theme-primary-color;
+  text-align: center;
+  vertical-align: middle;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  padding: 0.75rem 0;
+
+  #inner-footer {
+    margin: 0 3rem;
+  }
+}
+
+</style>
