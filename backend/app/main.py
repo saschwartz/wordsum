d01d52022b71b58485aa9f7b3856beddd627a1cc
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2019-07-18 13:51:16 +1000

    production conf

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,4 +9,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('FLASK_PORT', 8000), host='0.0.0.0')
+    app.run(debug=os.getenv('FLASK_DEBUG') is not None, port=os.getenv('FLASK_PORT', 8000), host='0.0.0.0')
